c3b81b8807f73831c77d5bd686ec7c8400f5a55e
Jacob Smith <jacoblsmith111@gmail.com>
2025-10-31 10:37:56 -0500

    Add normal ghost image

---
+++ github setup.R
@@ -0,0 +1,18 @@
+library(usethis)
+
+# First register for a github account
+
+#create_github_token()
+#gitcreds::gitcreds_set()
+#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
+
+Token: 
+  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
+
+# to push a git repo to github:
+use_github()
+
+# I'm going to start a new project first.
+
+# Let's make a change or two. I'll add that ghost cartoon
+
